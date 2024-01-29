31311163a18d594d9c5618f38f56443e9b567c2f
jmpkc <157860581+jmpkc@users.noreply.github.com>
2024-01-29 15:57:55 -0600

    Add files via upload

---
+++ frmMain.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        bSetColor = New Button()
+        tmrLightControl = New Timer(components)
+        txtColor = New TextBox()
+        Label1 = New Label()
+        lblTeamsStatus = New Label()
+        tmrCheckTeams = New Timer(components)
+        lblTeamsColor = New Label()
+        bTestButton = New Button()
+        tmrBlinkControl = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' bSetColor
+        ' 
+        bSetColor.Location = New Point(24, 28)
+        bSetColor.Name = "bSetColor"
+        bSetColor.Size = New Size(101, 39)
+        bSetColor.TabIndex = 0
+        bSetColor.Text = "Set Color"
+        bSetColor.UseVisualStyleBackColor = True
+        ' 
+        ' tmrLightControl
+        ' 
+        tmrLightControl.Enabled = True
+        tmrLightControl.Interval = 1000
+        ' 
+        ' txtColor
+        ' 
+        txtColor.Location = New Point(24, 73)
+        txtColor.Name = "txtColor"
+        txtColor.Size = New Size(100, 23)
+        txtColor.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(148, 28)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(78, 15)
+        Label1.TabIndex = 5
+        Label1.Text = "Teams Status:"
+        ' 
+        ' lblTeamsStatus
+        ' 
+        lblTeamsStatus.Location = New Point(232, 28)
+        lblTeamsStatus.Name = "lblTeamsStatus"
+        lblTeamsStatus.Size = New Size(184, 90)
+        lblTeamsStatus.TabIndex = 6
+        lblTeamsStatus.Text = "NoData"
+        ' 
+        ' tmrCheckTeams
+        ' 
+        tmrCheckTeams.Enabled = True
+        tmrCheckTeams.Interval = 3000
+        ' 
+        ' lblTeamsColor
+        ' 
+        lblTeamsColor.BackColor = Color.Black
+        lblTeamsColor.Location = New Point(168, 52)
+        lblTeamsColor.Name = "lblTeamsColor"
+        lblTeamsColor.Size = New Size(33, 25)
+        lblTeamsColor.TabIndex = 7
+        ' 
+        ' bTestButton
+        ' 
+        bTestButton.Location = New Point(26, 164)
+        bTestButton.Name = "bTestButton"
+        bTestButton.Size = New Size(99, 32)
+        bTestButton.TabIndex = 8
+        bTestButton.Text = "Test"
+        bTestButton.UseVisualStyleBackColor = True
+        ' 
+        ' tmrBlinkControl
+        ' 
+        tmrBlinkControl.Interval = 1000
+        ' 
+        ' frmMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(420, 259)
+        Controls.Add(bTestButton)
+        Controls.Add(lblTeamsColor)
+        Controls.Add(lblTeamsStatus)
+        Controls.Add(Label1)
+        Controls.Add(txtColor)
+        Controls.Add(bSetColor)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "frmMain"
+        Text = "BlinkStick Teams Status"
+        ResumeLayout(False)
+        PerformLayout()
+        'Me.Text = "Form1"
+    End Sub
+
+    Friend WithEvents bSetColor As Button
+    Friend WithEvents tmrLightControl As Timer
+    Friend WithEvents txtColor As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTeamsStatus As Label
+    Friend WithEvents tmrCheckTeams As Timer
+    Friend WithEvents lblTeamsColor As Label
+    Friend WithEvents bTestButton As Button
+    Friend WithEvents tmrBlinkControl As Timer
+
+
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,186 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Threading
+Imports System.Windows.Forms.DataFormats
+Imports BlinkStickDotNet
+
+
+Public Class frmMain
+    Dim BS As BlinkStick
+    Dim sColor As String = ""
+    Dim sColorOld As String = ""
+    Dim sTeamsStatus = ""
+    Dim sUserProfile As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+    Dim sTeamsLogFile As String = sUserProfile & "\AppData\Roaming\Microsoft\Teams\logs.txt"
+    Dim bBlinking As Boolean = False
+    Dim bBlinkingOld As Boolean = False
+
+    Private Sub bSetColor_Click(sender As Object, e As EventArgs) Handles bSetColor.Click
+        BS = BlinkStick.FindFirst()
+        If BS IsNot Nothing Then
+            If BS.OpenDevice() Then
+                'BS.SetMode(2)         'istn' working right.  hmm
+                'BS.SetColor("red")
+                'BS.Blink("red")
+                'mode    0 - Normal, 1 - Inverse, 2 - WS2812
+                'BS.Morph("red")
+                'BS.Pulse("red", 1, 1000, 50)
+                sColor = txtColor.Text
+                BS.SetColor(0, 0, sColor)
+                BS.SetColor(0, 1, sColor)
+            End If
+        Else
+            MsgBox("BlinkStick not found.", MsgBoxStyle.Critical, "BlinkStick for Teams")
+        End If
+    End Sub
+
+    Private Sub tmrLightConrol_Tick(sender As Object, e As EventArgs) Handles tmrLightControl.Tick
+        lblTeamsStatus.Text = sTeamsStatus
+        tmrLightControl.Enabled = False
+        If sColor IsNot sColorOld Then
+            sColorOld = sColor
+            BS = BlinkStick.FindFirst()
+            If BS IsNot Nothing Then
+                If BS.OpenDevice() Then
+                    BS.SetColor(0, 0, sColor)
+                    BS.SetColor(0, 1, sColor)
+                End If
+            End If
+        End If
+        If bBlinking = True Then
+            tmrBlinkControl.Enabled = True
+        Else
+            tmrBlinkControl.Enabled = False
+        End If
+    End Sub
+
+    Private Sub subCheckTeams()
+        Dim logFileStream As New FileStream(sTeamsLogFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        Dim logFileReader As New StreamReader(logFileStream)
+        Dim sLogLine As String
+        Dim sStatusLine As String   'StatusIndicatorStateService
+
+        While Not logFileReader.EndOfStream
+            sLogLine = logFileReader.ReadLine()
+            If InStr(sLogLine, "StatusIndicatorStateService") Then
+                sStatusLine = sLogLine
+            End If
+        End While
+
+        If InStr(sStatusLine, "Available") Then
+            sTeamsStatus = "Available"
+            bBlinking = False
+            sColor = "green"
+            lblTeamsColor.BackColor = Color.Green
+
+        ElseIf InStr(sStatusLine, "Away") Then
+            sTeamsStatus = "Away"
+            bBlinking = False
+            sColor = "black"
+            lblTeamsColor.BackColor = Color.Black
+
+        ElseIf InStr(sStatusLine, "OnThePhone") Then
+            sTeamsStatus = "OnThePhone"
+            bBlinking = True
+            sColor = "red"
+            lblTeamsColor.BackColor = Color.Red
+
+        ElseIf InStr(sStatusLine, "InAMeeting") Then
+            sTeamsStatus = "InAMeeting"
+            bBlinking = False
+            sColor = "red"
+            lblTeamsColor.BackColor = Color.Red
+
+        ElseIf InStr(sStatusLine, "DoNotDisturb") Then
+            sTeamsStatus = "DoNotDisturb"
+            bBlinking = True
+            sColor = "red"
+            lblTeamsColor.BackColor = Color.Red
+
+        ElseIf InStr(sStatusLine, "Presenting") Then
+            sTeamsStatus = "Presenting"
+            bBlinking = True
+            sColor = "red"
+            lblTeamsColor.BackColor = Color.Red
+
+        ElseIf InStr(sStatusLine, "Busy") Then
+            sTeamsStatus = "Busy"
+            bBlinking = False
+            sColor = "red"
+            lblTeamsColor.BackColor = Color.Red
+
+        ElseIf InStr(sStatusLine, "Offline") Then
+            sTeamsStatus = "Offline"
+            bBlinking = False
+            sColor = "black"
+            lblTeamsColor.BackColor = Color.Black
+
+        ElseIf InStr(sStatusLine, "BeRightBack") Then
+            sTeamsStatus = "BeRightBack"
+            bBlinking = True
+            sColor = "purple"
+            lblTeamsColor.BackColor = Color.Purple
+
+        Else
+            sTeamsStatus = sStatusLine
+            bBlinking = False
+            sColor = "black"
+            lblTeamsColor.BackColor = Color.Black
+
+        End If
+
+        lblTeamsStatus.Text = sTeamsStatus
+        If sColor = sColorOld And bBlinking = bBlinkingOld Then
+        Else
+            tmrLightControl.Enabled = True
+        End If
+
+        logFileReader = Nothing
+        logFileStream = Nothing
+
+    End Sub
+    Private Sub tmrCheckTeams_Tick(sender As Object, e As EventArgs) Handles tmrCheckTeams.Tick
+        subCheckTeams()
+    End Sub
+
+    Private Sub frmMain_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
+        BS = BlinkStick.FindFirst()
+        If BS IsNot Nothing Then
+            If BS.OpenDevice() Then
+                BS.SetColor(0, 0, "black")
+                BS.SetColor(0, 1, "black")
+                BS.TurnOff()
+            End If
+        End If
+    End Sub
+
+    Private Sub bTestButton_Click(sender As Object, e As EventArgs) Handles bTestButton.Click
+        BS = BlinkStick.FindFirst()
+        If BS IsNot Nothing Then
+            If BS.OpenDevice() Then
+                'BS.SetColor(0, 0, "red")
+                'BS.SetColor(0, 1, "red")
+
+                'Thread.Sleep(1000)
+                BS.Blink(0, 0, "purple")
+                BS.Blink(0, 1, "purple")
+                'mode    0 - Normal, 1 - Inverse, 2 - WS2812
+                'BS.Morph("red")
+                'BS.Pulse("red", 1, 1000, 50)
+            End If
+        End If
+    End Sub
+
+    Private Sub tmrBlinkControl_Tick(sender As Object, e As EventArgs) Handles tmrBlinkControl.Tick
+        bBlinkingOld = bBlinking
+        BS = BlinkStick.FindFirst()
+        If BS IsNot Nothing Then
+            If BS.OpenDevice() Then
+                BS.Blink(sColor)
+                'mode    0 - Normal, 1 - Inverse, 2 - WS2812
+                'BS.Morph("red")
+                'BS.Pulse("red", 1, 1000, 50)
+            End If
+        End If
+    End Sub
+End Class
